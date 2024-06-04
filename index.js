bb6c64e66cfa8421d8cb48ca892e9a536c2bd18d
NitinametaDevops <135969603+NitinametaDevops@users.noreply.github.com>
2024-06-04 15:50:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 4000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World How are you!');
+  res.send('Hello World How are !');
 });
 
 app.listen(port, () => {
